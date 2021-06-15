0c5d2005f4bbcc8e23a5fe96dbb4e193f88d6b7f
Anthony-Quere <anthony.quere@decathlon.com>
2021-06-15 09:37:50 +0200

    Add sed inplace modifications

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
 
 setup_requirement_file() {
-	echo "Authentication with github ssh key"
+	echo "> Authentication with github token"
 
 	TOKEN=${INPUT_AUTH_SSH_KEY}
 
-	sed "s/https:\/\/github\.com/https:\/\/$TOKEN:github\.com/1" app/requirements.txt > temp_requirements.txt
-	sed "s/ssh:\/\/github\.com/https:\/\/$TOKEN:github\.com/1" temp_requirements.txt > temp_requirements.txt
+	sed -i "s/https:\/\/github\.com/https:\/\/$TOKEN:github\.com/1" app/requirements.txt
+	sed -i "s/ssh:\/\/github\.com/https:\/\/$TOKEN:github\.com/1" app/requirements.txt
 }
 
 add_requirements() {
 	if [ -f "app/requirements.txt" ]
 	then
+		echo "> Setup requirement file..."
 		setup_requirement_file
-		echo "Installing requirements..."
-		echo "> cat app/requirements.txt"
-		cat app/requirements.txt
-
-		echo "> cat temp_requirements.txt"
-		cat temp_requirements.txt
+		echo "> Installing requirements..."
 
 		mkdir -p libs
-		pip install -vvv --target app -r temp_requirements.txt
+		pip install -vvv --target libs -r temp_requirements.txt
 		# pip install -vvv --target libs -r temp_requirements.txt
 		if [ $? -ne 0 ]
 		then
-			echo "Fail to add requirements"
+			echo "> Fail to add requirements"
 			exit 1
 		fi
     fi
 }
 
 deploy_function() {
-	echo "Deploying function ..."
+	echo "> Deploying function ..."
 	RETCODE=0
 	cd "${INPUT_WORKING_DIRECTORY}"
 	add_requirements
@@ -41,12 +37,12 @@ deploy_function() {
 		--timeout "${INPUT_TIMEOUT}" --memory-size "${INPUT_MEMORY}" --role "${INPUT_ROLE}" \
 		--handler "${INPUT_HANDLER}" ${OPT_ENV_VARIABLES} ${OPT_VPC_CONFIG} --zip-file fileb://code.zip
     RETCODE=$((RETCODE+$?))
-	[ $RETCODE -ne 0 ] && echo "ERROR : failed to create the function."
+	[ $RETCODE -ne 0 ] && echo "> ERROR : failed to create the function."
 	exit $RETCODE
 }
 
 update_function() {
-	echo "Updating function ..."
+	echo "> Updating function ..."
 	RETCODE=0
 	cd "${INPUT_WORKING_DIRECTORY}"
 	# add_requirements
@@ -57,7 +53,7 @@ update_function() {
     RETCODE=$((RETCODE+$?))
 	aws lambda update-function-code --function-name "${INPUT_FUNCTION_NAME}" --zip-file fileb://code.zip
     RETCODE=$((RETCODE+$?))
-	[ $RETCODE -ne 0 ] && echo "ERROR : failed to update the function."
+	[ $RETCODE -ne 0 ] && echo "> ERROR : failed to update the function."
 	exit $RETCODE
 }
 
@@ -71,32 +67,32 @@ deploy_or_update_function() {
     then 
             OPT_VPC_CONFIG="--vpc-config ${INPUT_VPC_CONFIG}"
     fi
-    echo "Checking function existence..."
+    echo "> Checking function existence..."
 	aws lambda get-function --function-name "${INPUT_FUNCTION_NAME}" &> /dev/null
 	if [ $? != 0 ]
 	then
-		echo "Function ${INPUT_FUNCTION_NAME} not found, running initial deployment..."
+		echo "> Function ${INPUT_FUNCTION_NAME} not found, running initial deployment..."
 		deploy_function
 	else
-		echo "Function found, updating..."
+		echo "> Function found, updating..."
 		update_function
     fi
-    echo "Done."
+    echo "> Done."
 }
 
 show_environment() {
-	echo "Function name: ${INPUT_FUNCTION_NAME}"
-	echo "Runtime: ${INPUT_RUNTIME}"
-	echo "Memory size: ${INPUT_MEMORY}"
-	echo "Timeout: ${INPUT_TIMEOUT}"
-	echo "IAM role: ${INPUT_ROLE}"
-	echo "Lambda handler: ${INPUT_HANDLER}"
-	echo "Working directory: ${INPUT_WORKING_DIRECTORY}"
-	echo "Environment variables: ${INPUT_ENV_VARIABLES}"
-	echo "VPC Config: ${INPUT_VPC_CONFIG}"
+	echo "> Function name: ${INPUT_FUNCTION_NAME}"
+	echo "> Runtime: ${INPUT_RUNTIME}"
+	echo "> Memory size: ${INPUT_MEMORY}"
+	echo "> Timeout: ${INPUT_TIMEOUT}"
+	echo "> IAM role: ${INPUT_ROLE}"
+	echo "> Lambda handler: ${INPUT_HANDLER}"
+	echo "> Working directory: ${INPUT_WORKING_DIRECTORY}"
+	echo "> Environment variables: ${INPUT_ENV_VARIABLES}"
+	echo "> VPC Config: ${INPUT_VPC_CONFIG}"
 }
 
-echo "dpolombo/action-deploy-aws-lambda@v1.6"
+echo "Anthony-Quere/action-deploy-aws-lambda@v1.6"
 aws --version
 show_environment
 add_requirements
